0266d61e634f9fb16c00e5a8c84ac5065eb7c616
Alcash <Pirynas@mail.ru>
2021-07-14 17:10:00 +0700

    ограничение в добавление элементов. вью для кнопок

--- Assets/Scripts/Transport/TransportManager.cs
+++ Assets/Scripts/Transport/TransportManager.cs
@@ -1,9 +1,11 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TransportManager : MonoBehaviour
 {
+    public event Action OnChangeStatus;
+
     [SerializeField]
     private Transform workPlaceRoot;
     [SerializeField]
@@ -18,20 +20,32 @@ public class TransportManager : MonoBehaviour
     [SerializeField]
     private float waitToNewElement = 0.5f;
 
-    public bool CanMoveToLine { get; protected set; }
+    public bool CanMoveToLine { get; protected set; } = true;
+    public bool CanAddItem { get; protected set; } = true;
+    private int itemOnElementCount = 0;
+    [SerializeField]
+    private int maxItemOnElement = 3;   
 
     public void AddItem(ItemsSO itemsSO)
     {
-        if(workLineElement != null)
-          itemPlace.AddItem(itemsSO);
+        if (workLineElement != null && CanAddItem)
+        {
+            itemPlace.AddItem(itemsSO);
+            itemOnElementCount++;
+            CanAddItem = itemOnElementCount < maxItemOnElement;
+            OnChangeStatus();
+        }
     }
 
     private void CreateTransportElement()
     {
+       
         itemPlace = new ItemPlace();
         workLineElement = Instantiate(lineElementPrefab, workPlaceRoot);
         workLineElement.SetItem(itemPlace);
-
+        itemOnElementCount = 0;
+        CanAddItem = true;
+        OnChangeStatus();
     }
 
     private void Start()
@@ -45,14 +59,18 @@ public class TransportManager : MonoBehaviour
         {
             transportLine.AddElements(workLineElement);
             workLineElement = null;
+            CanMoveToLine = false;
+            CanAddItem = false;
             Invoke(nameof(CreateTransportElement), 0.1f);
             Invoke(nameof(CanMove), waitToNewElement);
+            OnChangeStatus();
         }
     }
 
     private void CanMove()
     {
         CanMoveToLine = true;
+        OnChangeStatus();
     }
 
     
