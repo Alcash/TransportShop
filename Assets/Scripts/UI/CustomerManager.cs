5cdba4afb41bccc5867022cd54db25e8e512564e
Alcash <Pirynas@mail.ru>
2021-07-15 22:02:37 +0700

    измение времени ожидания

--- Assets/Scripts/UI/CustomerManager.cs
+++ Assets/Scripts/UI/CustomerManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class CustomerManager : MonoBehaviour
+public class CustomerManager : MonoBehaviour, IUpdatable
 {
 
     [SerializeField]
@@ -19,6 +19,8 @@ public class CustomerManager : MonoBehaviour
 
     private Dictionary<Transform, CustomerController> customerPoint = new Dictionary<Transform, CustomerController>();
 
+    private float timer = 0;
+
     private void Awake()
     {
         SpawnCustomers();
@@ -27,6 +29,7 @@ public class CustomerManager : MonoBehaviour
     private void OnEnable()
     {
         LetsGoCustomer();
+        UpdateManager.AddUpdateObject(this);
     }
 
     private void OnDisable()
@@ -37,7 +40,9 @@ public class CustomerManager : MonoBehaviour
                  customerPoint[item].gameObject.SetActive(false);
             customerPoint[item] = null;
         }
-       
+
+        UpdateManager.RemoveUpdateObject(this);
+
     }
 
     private void SpawnCustomers()
@@ -47,9 +52,7 @@ public class CustomerManager : MonoBehaviour
         {
             customers[i] = Instantiate(prefabCustomer,transform.position, Quaternion.identity);
             customers[i].gameObject.SetActive(false);
-            customers[i].OnReturned += CustomerReturned;
-
-            
+            customers[i].OnReturned += CustomerReturned;            
         }      
 
         for (int i = 0; i < poointInLine.Length; i++)
@@ -89,9 +92,9 @@ public class CustomerManager : MonoBehaviour
             customers[indexCustomer].transform.position = transform.position;
             customers[indexCustomer].SetPoint(poointInLine[rndLine]);
             customers[indexCustomer].gameObject.SetActive(true);
-
-            Invoke(nameof(LetsGoCustomer), Random.Range(timerMin, timerMax));
+            timer = Random.Range(timerMin, timerMax);
         }
+        
     }
 
     private void CustomerReturned(CustomerController customerController)
@@ -99,11 +102,8 @@ public class CustomerManager : MonoBehaviour
         
         var lineKey = customerPoint.FirstOrDefault(x => x.Value == customerController).Key;
         customerPoint[lineKey] = null;       
-        customerController.gameObject.SetActive(false);      
-        Invoke(nameof(LetsGoCustomer), Random.Range(timerMin, timerMax));
-    }
-
-   
+        customerController.gameObject.SetActive(false);   
+    }   
 
     private void OnDestroy()
     {
@@ -111,5 +111,14 @@ public class CustomerManager : MonoBehaviour
         {
             item.OnReturned -= CustomerReturned;
         }       
+    }
+
+    void IUpdatable.DoUpdate(float delta)
+    {
+        timer -= delta;
+        if (timer <= 0)
+        {
+            LetsGoCustomer();
+        }
     }
 }
